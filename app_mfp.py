83343aaaf3eb7b90ae97eb828e4f2fc87ae39caa
KasperTak <118729147+KasperTak@users.noreply.github.com>
2024-02-20 18:51:06 +0100

    Update app_mfp.py

--- app_mfp.py
+++ app_mfp.py
@@ -61,6 +61,7 @@ with tab1:
     studieschuldtabel = pd.DataFrame(data_4,columns=columns_4)
     
     studieschuldtabel['Debetrente'] = studieschuldtabel['Debetrente'].apply(lambda x: f"{x:.3f}".replace('.',','))
+    VOOR_AOW['toetsinkomen'] = VOOR_AOW['toetsinkomen'].str.replace('"', '').str.replace(',', '').astype(int)
     st.write(VOOR_AOW)
     
     with column1:
